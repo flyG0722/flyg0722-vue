46150bff7be9d2d2a11081b481c80ea8358a044d
chen <gc2357119339@163.com>
2024-07-24 15:34:46 +0800

    project init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style lang="scss">
+  body {
+    background: #f1f3fa;
+  }
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="home-container">
+      123
+      <BlogHeaderVue></BlogHeaderVue>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import BlogHeaderVue from "@/components/BlogHeader.vue";
+</script>
+
+<style scoped lang="scss">
+  .home-container {
+    width: 60vw;
+    margin: 0 auto;
+    background: red;
+  }
+</style>
